8041ea8a688f68caddf6fcabff2d765ac24b42bb
Calfur <robin.nater@hotmail.com>
2022-10-10 21:40:23 +0200

    Increase swipe speed from 4.5 to 5.5

--- UnityProject/Assets/Scripts/Configurations.cs
+++ UnityProject/Assets/Scripts/Configurations.cs
@@ -3,7 +3,7 @@
     public static class Configurations
     {
         public static readonly int lastLevel = 50;
-        public static readonly float swipeSpeed = (float)4.5;
+        public static readonly float swipeSpeed = (float)5.5;
         public static readonly int startCoins = 3;
         public static readonly string levelMapsFolder = "LevelMaps";
         public static readonly string solutionsFolder = "Solutions";
